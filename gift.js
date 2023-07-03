2b213882473ce946993614432deb47cbd9a7aecf
Flavia Kosti <flaviakosti@gmail.com>
2023-07-03 15:12:20 +0200

    Add candles

--- gift.js
+++ gift.js
@@ -1,7 +1,16 @@
 function openGift() {
     gift.style.display = 'none';
     confeti.style.display = 'block';
-    cake.style.display='block';
+    cake.style.display = 'block';
+    bdcake.style.display = 'block';
+    candle.style.display = 'block';
+    candle1.style.display = 'block';
+    candle2.style.display = 'block';
+    candle3.style.display = 'block';
+    animation1.setAttribute('begin', '2s');
     var audio = new Audio('happy-birthday.mp3');
-    //  audio.play();
+    setTimeout(() => {
+           audio.play(); 
+    }, 4500) 
+
 }
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const confeti = document.getElementById('confetti');
 const gift = document.getElementById('suprise-gift');
 const cake = document.getElementById('bday-cake');
-
+const candle = document.getElementById('candle');
+const candle1 = document.getElementById('candle1');
+const candle2 = document.getElementById('candle2');
+const candle3 = document.getElementById('candle3');
+const bdcake = document.getElementById('cake')
+const animation1 = document.getElementById('bizcocho_1');
 
 gift.addEventListener('click', openGift);
