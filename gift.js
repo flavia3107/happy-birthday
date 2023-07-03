6caca02297ddd4768b0f5d43de95a673deebaee5
Flavia Kosti <flaviakosti@gmail.com>
2023-07-03 12:35:29 +0200

    Add cake

---
+++ gift.js
@@ -0,0 +1,7 @@
+function openGift() {
+    gift.style.display = 'none';
+    confeti.style.display = 'block';
+    cake.style.display='block';
+    var audio = new Audio('happy-birthday.mp3');
+    //  audio.play();
+}
--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const confeti = document.getElementById('confetti');
 const gift = document.getElementById('suprise-gift');
+const cake = document.getElementById('bday-cake');
 
-console.log(confeti)
 
- window.addEventListener('load', function() {
-      var audio = new Audio('happy-birthday.mp3');
-    //   audio.play();
-    });
+gift.addEventListener('click', openGift);
