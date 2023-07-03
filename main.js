ea875dce7fa8a4d84aeba1940f1e584de0cdfe0e
Flavia Kosti <flaviakosti@gmail.com>
2023-07-03 11:53:49 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const confeti = document.getElementById('confetti');
+const gift = document.getElementById('suprise-gift');
+
+console.log(confeti)
+
+ window.addEventListener('load', function() {
+      var audio = new Audio('happy-birthday.mp3');
+    //   audio.play();
+    });
