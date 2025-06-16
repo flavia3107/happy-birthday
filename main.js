0df81573a5b56741d0f9cbfec769c08ba84164b0
Flavia Kosti <flaviakosti@gmail.com>
2025-06-15 21:41:04 -0400

    Birthday: Fix P1

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ const bdcake = document.getElementById('cake')
 const animation1 = document.getElementById('bizcocho_1');
 const container = document.getElementById('container');
 gift.addEventListener('click', openGift);
+
+
+function openGift() {
+	gift.style.display = 'none';
+	confeti.style.display = 'block';
+	cake.style.display = 'block';
+	bdcake.style.display = 'block';
+	candle.style.display = 'block';
+	candle1.style.display = 'block';
+	candle2.style.display = 'block';
+	candle3.style.display = 'block';
+	animation1.setAttribute('begin', '2s');
+	var audio = new Audio('happy-birthday.mp3');
+
+	setTimeout(() => {
+		container.style.visibility = 'visible';
+	}, 3500);
+
+	setTimeout(() => {
+		audio.play();
+	}, 4500);
+
+}
